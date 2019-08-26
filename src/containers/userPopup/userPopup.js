3fc364634997a287042f02ec956291b70acf0f40
Turing School <turingschool@Turings-MacBook-Pro.local>
2019-08-26 17:59:36 -0600

    Convert to class compoenent and conditionally render output

--- src/containers/userPopup/userPopup.js
+++ src/containers/userPopup/userPopup.js
@@ -1,58 +1,82 @@
-import React, { Fragment } from "react";
-import { withRouter } from "react-router-dom";
+import React, { Component, Fragment } from "react";
 import "./userPopup.scss";
 
-function userPopup(props) {
-  const handleExit = () => {
-    props.history.push("/");
+class userPopup extends Component {
+  state = {
+    username: "",
+    password: ""
   };
 
-  const handlePageChange = destination => props.history.push(`/${destination}`);
+  handleExit = () => {
+    this.props.history.push("/");
+  };
 
-  const loginFormat = (
-    <Fragment>
-      <h3 className="editor-title">To continue, log in to PalettePicker.</h3>
-      <label htmlFor="">Enter Username</label>
-      <input type="text" name="username" />
-      <label htmlFor="">Enter Password</label>
-      <input type="password" name="password" />
-      <button className="submit-btn">Login</button>
-      <button
-        className="submit-btn"
-        onClick={() => handlePageChange("register")}
-      >
-        new user?
-      </button>
-    </Fragment>
-  );
+  handlePageChange = destination => this.props.history.push(`/${destination}`);
 
-  const registerFormat = (
-    <Fragment>
-      <h3 className="editor-title">Sign up with a username and password.</h3>
-      <label htmlFor="">Enter Username</label>
-      <input type="email" name="email" />
-      <label htmlFor="">Enter Password</label>
-      <input type="text" name="password" />
-      <button className="submit-btn">Register</button>
-      <button className="submit-btn" onClick={() => handlePageChange("login")}>
-        Already have an account?
-      </button>
-    </Fragment>
-  );
+  handleLogin = e => {
+    console.log("logging in...");
+    e.preventDefault();
+    // login user
+  };
 
-  return (
-    <Fragment>
-      <div className="screen" />
-      <section className="SavePopup">
-        <button className="editor-exit" onClick={handleExit}>
-          X
-        </button>
-        <form className="editor-form">
-          {props.location.pathname === "/login" ? loginFormat : registerFormat}
+  handleRegister = e => {
+    console.log("Registering...");
+    e.preventDefault();
+    // register user
+  };
+
+  render() {
+    const loginFormat = (
+      <Fragment>
+        <h3 className="editor-title">To continue, log in to PalettePicker.</h3>
+        <form onSubmit={e => this.handleLogin(e)}>
+          <label htmlFor="">Enter Username</label>
+          <input type="text" name="username" />
+          <label htmlFor="">Enter Password</label>
+          <input type="password" name="password" />
+          <button className="submit-btn">Login</button>
+          <button
+            className="submit-btn"
+            onClick={() => this.handlePageChange("register")}
+          >
+            new user?
+          </button>
         </form>
-      </section>
-    </Fragment>
-  );
+      </Fragment>
+    );
+
+    const registerFormat = (
+      <Fragment>
+        <h3 className="editor-title">Sign up with a username and password.</h3>
+        <form onSubmit={e => this.handleRegister(e)}>
+          <label htmlFor="">Enter Username</label>
+          <input type="email" name="email" />
+          <label htmlFor="">Enter Password</label>
+          <input type="text" name="password" />
+          <button className="submit-btn">Register</button>
+          <button
+            className="submit-btn"
+            onClick={() => this.handlePageChange("login")}
+          >
+            Already have an account?
+          </button>
+        </form>
+      </Fragment>
+    );
+    return (
+      <Fragment>
+        <div className="screen" />
+        <section className="UserPopup">
+          <button className="popup-exit" onClick={this.handleExit}>
+            X
+          </button>
+          {this.props.location.pathname === "/login"
+            ? loginFormat
+            : registerFormat}
+        </section>
+      </Fragment>
+    );
+  }
 }
 
 export default userPopup;
