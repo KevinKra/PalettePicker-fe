e889f268b0584e0481ec35cf87a34ea581af48cf
Turing School <turingschool@Turings-MacBook-Pro.local>
2019-08-26 19:02:24 -0600

    Correct styling import path

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { withRouter } from 'react-router-dom';
-import "./NavBar.css";
+import { withRouter } from "react-router-dom";
+import "./NavBar.scss";
 
 function NavBar(props) {
-  const handleClick = (e,destination) => {
+  const handleClick = (e, destination) => {
     e.preventDefault();
-    props.history.push(`/${destination}`)
-  }
+    props.history.push(`/${destination}`);
+  };
   return (
     <header className="NavBar">
       <h2>PalettePicker</h2>
       <form className="navbar-form">
-        <button onClick={(e) => handleClick(e,"login")}>Login</button>
-        <button onClick={(e) => handleClick(e,"register")}>Register</button>
+        <button onClick={e => handleClick(e, "login")}>Login</button>
+        <button onClick={e => handleClick(e, "register")}>Register</button>
       </form>
     </header>
   );
