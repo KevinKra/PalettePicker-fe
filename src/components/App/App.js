f62ac7b1cdd986df05fa36c521b8698bc9318ea8
Turing School <turingschool@Turings-MacBook-Pro.local>
2019-08-26 18:59:01 -0600

    Semantic correction div > section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,18 +5,18 @@ import NavBar from "../NavBar/NavBar";
 import PaletteEditor from "../PaletteEditor/PaletteEditor";
 import SavePopup from "../../containers/SavePopup/SavePopup";
 import userPopup from "../../containers/userPopup/userPopup";
-import { Route, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <section className="App">
       <Route path="/" component={NavBar} />
       <Route path="/" component={LandingPage} />
       <Route exact path="/edit-palette" component={PaletteEditor} />
       <Route exact path="/save-palette" component={SavePopup} />
       <Route exact path="/login" component={userPopup} />
       <Route exact path="/register" component={userPopup} />
-    </div>
+    </section>
   );
 }
 
