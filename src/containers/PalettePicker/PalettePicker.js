3f8747f94cab00196ea87345648b6cf14b4295f3
Kevin Krato <kratok08@gmail.com>
2019-12-13 13:15:54 -0700

    Make slight refactor adjustments and prettier modifications

--- src/containers/PalettePicker/PalettePicker.js
+++ src/containers/PalettePicker/PalettePicker.js
@@ -1,177 +1,168 @@
-import React, { Component } from "react";
-import ColorScheme from "color-scheme";
-import ColorBar from "../../components/ColorBar/ColorBar";
-import EditBarFull from "../../components/EditBarFull/EditBarFull";
-import EditBarPartial from "../../components/EditBarPartial/EditBarPartial";
-import { withRouter } from "react-router-dom";
-import * as actions from "../../_redux/actions";
-import { connect } from "react-redux";
-import "./PalettePicker.css";
+import React, { Component } from 'react';
+import ColorScheme from 'color-scheme';
+import ColorBar from '../../components/ColorBar/ColorBar';
+import EditBarFull from '../../components/EditBarFull/EditBarFull';
+import EditBarPartial from '../../components/EditBarPartial/EditBarPartial';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../../_redux/actions';
+import { connect } from 'react-redux';
+import './PalettePicker.scss';
 
 class PalettePicker extends Component {
-  state = {
-    hue: "",
-    hueLocked: false,
-    colorScheme: "triade",
-    variation: "pastel",
-    colors: [],
-    editable: false
-  };
+	state = {
+		hue: '',
+		hueLocked: false,
+		colorScheme: 'triade',
+		variation: 'pastel',
+		colors: [],
+		editable: false
+	};
 
-  componentDidMount() {
-    this.generateColors();
-  }
+	componentDidMount() {
+		this.generateColors();
+	}
 
-  componentDidUpdate() {
-    this.props.updateCurrentPalette(this.state.colors);
-  }
+	componentDidUpdate() {
+		this.props.updateCurrentPalette(this.state.colors);
+	}
 
-  saveDialogOpen = () => {
-    this.props.history.push("save-palette");
-  };
+	saveDialogOpen = () => {
+		this.props.history.push('save-palette');
+	};
 
-  toggleEditable = () => {
-    const toggle = this.state.editable;
-    this.setState({ editable: !toggle });
-  };
+	toggleEditable = () => {
+		const toggle = this.state.editable;
+		this.setState({ editable: !toggle });
+	};
 
-  updateHue = e => {
-    const { value } = e.target;
-    this.setState({ hue: value });
-  };
+	updateHue = (e) => {
+		const { value } = e.target;
+		this.setState({ hue: value });
+	};
 
-  hueLock = () => {
-    this.setState({ hueLocked: true });
-  };
+	hueLock = () => {
+		this.setState({ hueLocked: true });
+	};
 
-  hueUnlock = () => {
-    this.setState({ hueLocked: false });
-  };
+	hueUnlock = () => {
+		this.setState({ hueLocked: false });
+	};
 
-  updateColorScheme = e => {
-    const { value } = e.target;
-    console.log(value);
-    this.setState({ colorScheme: value });
-  };
+	updateColorScheme = (e) => {
+		const { value } = e.target;
+		console.log(value);
+		this.setState({ colorScheme: value });
+	};
 
-  updateVariation = e => {
-    const { value } = e.target;
-    this.setState({ variation: value });
-  };
+	updateVariation = (e) => {
+		const { value } = e.target;
+		this.setState({ variation: value });
+	};
 
-  updateColors = (e, previousColors) => {
-    e.preventDefault();
-    this.generateColors(previousColors);
-  };
+	updateColors = (e, previousColors) => {
+		e.preventDefault();
+		this.generateColors(previousColors);
+	};
 
-  generateRandomHue = () => {
-    return Math.floor(Math.random() * (360 + 1));
-  };
+	generateRandomHue = () => {
+		return Math.floor(Math.random() * (360 + 1));
+	};
 
-  handleLockStatus = (targetColor, lockStatus) => {
-    const colorIndex = this.state.colors.findIndex(color => {
-      return targetColor === color;
-    });
-    const colors = this.state.colors.slice();
-    colors[colorIndex].locked = lockStatus;
-    this.setState({ colors });
-  };
+	handleLockStatus = (targetColor, lockStatus) => {
+		const colorIndex = this.state.colors.findIndex((color) => {
+			return targetColor === color;
+		});
+		const colors = this.state.colors.slice();
+		colors[colorIndex].locked = lockStatus;
+		this.setState({ colors });
+	};
 
-  generateColors = async (previousColors = []) => {
-    // The possible values are 'mono', 'contrast', 'triade', 'tetrade', and 'analogic'
-    const { hue, hueLocked, colorScheme, variation, colors } = this.state;
-    const { propColorScheme = "triade" } = this.props;
+	generateColors = async (previousColors = []) => {
+		// The possible values are 'mono', 'contrast', 'triade', 'tetrade', and 'analogic'
+		const { hue, hueLocked, colorScheme, variation, colors } = this.state;
+		const { propColorScheme = 'triade' } = this.props;
 
-    const hueToUse = !hueLocked ? this.generateRandomHue() :  hue;
-  
-    // const hueToUse = generatedHue || hue;
-    const colorSchemeToUse = colorScheme || propColorScheme; 
+		const hueToUse = !hueLocked ? this.generateRandomHue() : hue;
 
-    // instantiate the ColorScheme package with our define state and props
-    const scheme = new ColorScheme();
-    scheme
-      .from_hue(hueToUse)
-      .scheme(colorSchemeToUse)
-      .variation(variation);
-    // generate array of objects to be used for palette board
-    const generatedColors = scheme.colors().map(color => {
-      return { hex: "#" + color, locked: false };
-    });
-    // handling new palette generation
-    if (previousColors.length !== generatedColors.length) {
-      // if the previous/current palettes are a different size, generate an entirely new palette
-      this.setState({ colors: generatedColors, hue: hueToUse });
-    } else {
-      // if the previous/current palettes are the same size, update the current palette 
-      colors.forEach((color, i) => {
-        // but do not changed locked values
-        if (color.locked === true) generatedColors.splice(i, 1, color);
-      });
-      this.setState({ colors: generatedColors, hue: hueToUse });
-    }
-  };
+		// const hueToUse = generatedHue || hue;
+		const colorSchemeToUse = colorScheme || propColorScheme;
 
-  render() {
-    const colors = this.state.colors.map((color, i) => {
-      return (
-        <ColorBar
-          color={color}
-          vRotate={this.props.vRotate}
-          number={i}
-          handleLockStatus={this.handleLockStatus}
-          key={i}
-        />
-      );
-    });
+		// instantiate the ColorScheme package with our define state and props
+		const scheme = new ColorScheme();
+		scheme.from_hue(hueToUse).scheme(colorSchemeToUse).variation(variation);
+		// generate array of objects to be used for palette board
+		const generatedColors = scheme.colors().map((color) => {
+			return { hex: '#' + color, locked: false };
+		});
+		// handling new palette generation
+		if (previousColors.length !== generatedColors.length) {
+			// if the previous/current palettes are a different size, generate an entirely new palette
+			this.setState({ colors: generatedColors, hue: hueToUse });
+		} else {
+			// if the previous/current palettes are the same size, update the current palette
+			colors.forEach((color, i) => {
+				if (color.locked === true) generatedColors.splice(i, 1, color);
+			});
+			this.setState({ colors: generatedColors, hue: hueToUse });
+		}
+	};
 
-    return (
-      <section className="PalettePicker">
-        <EditBarPartial
-          editable={this.state.editable}
-          hue={this.state.hue}
-          hueLocked={this.state.hueLocked}
-          colorScheme={this.state.colorScheme}
-          variation={this.state.variation}
-          colors={this.state.colors}
-          updateColors={this.updateColors}
-          hueLock={this.hueLock}
-          hueUnlock={this.hueUnlock}
-        />
-        <EditBarFull
-          editable={this.state.editable}
-          hue={this.state.hue}
-          hueLocked={this.state.hueLocked}
-          colors={this.state.colors}
-          updateColors={this.updateColors}
-          updateHue={this.updateHue}
-          hueLock={this.hueLock}
-          hueUnlock={this.hueUnlock}
-          updateVariation={this.updateVariation}
-          updateColorScheme={this.updateColorScheme}
-        />
-        <div className="colors-section">{colors}</div>
-        <div className="button-bar">
-          <button className="primary-btn" onClick={this.toggleEditable}>
-            Edit
-          </button>
-          <button className="primary-btn" onClick={this.saveDialogOpen}>
-            Save
-          </button>
-        </div>
-      </section>
-    );
-  }
+	render() {
+		const colors = this.state.colors.map((color, i) => {
+			return (
+				<ColorBar
+					color={color}
+					vRotate={this.props.vRotate}
+					number={i}
+					handleLockStatus={this.handleLockStatus}
+					key={i}
+				/>
+			);
+		});
+
+		return (
+			<section className="PalettePicker">
+				<EditBarPartial
+					editable={this.state.editable}
+					hue={this.state.hue}
+					hueLocked={this.state.hueLocked}
+					colorScheme={this.state.colorScheme}
+					variation={this.state.variation}
+					colors={this.state.colors}
+					updateColors={this.updateColors}
+					hueLock={this.hueLock}
+					hueUnlock={this.hueUnlock}
+				/>
+				<EditBarFull
+					editable={this.state.editable}
+					hue={this.state.hue}
+					hueLocked={this.state.hueLocked}
+					colors={this.state.colors}
+					updateColors={this.updateColors}
+					updateHue={this.updateHue}
+					hueLock={this.hueLock}
+					hueUnlock={this.hueUnlock}
+					updateVariation={this.updateVariation}
+					updateColorScheme={this.updateColorScheme}
+				/>
+				<div className="colors-section">{colors}</div>
+				<div className="button-bar">
+					<button className="primary-btn" onClick={this.toggleEditable}>
+						Edit
+					</button>
+					<button className="primary-btn" onClick={this.saveDialogOpen}>
+						Save
+					</button>
+				</div>
+			</section>
+		);
+	}
 }
 
-const mapDispatchToProps = dispatch => ({
-  createNewProject: project => dispatch(actions.createNewProject(project)),
-  updateExistingProject: project =>
-    dispatch(actions.updateExistingProject(project)),
-  updateCurrentPalette: palette =>
-    dispatch(actions.updateCurrentPalette(palette))
+const mapDispatchToProps = (dispatch) => ({
+	createNewProject: (project) => dispatch(actions.createNewProject(project)),
+	updateExistingProject: (project) => dispatch(actions.updateExistingProject(project)),
+	updateCurrentPalette: (palette) => dispatch(actions.updateCurrentPalette(palette))
 });
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(withRouter(PalettePicker));
+export default connect(null, mapDispatchToProps)(withRouter(PalettePicker));
--- src/containers/ProjectsContainer/ProjectsContainer.js
+++ src/containers/ProjectsContainer/ProjectsContainer.js
@@ -1,137 +1,130 @@
-import React, { Component } from "react";
-import Project from "../../components/Project/Project";
-import { TransitionGroup } from "react-transition-group";
-import * as actions from "../../_redux/actions/index";
-import { connect } from "react-redux";
-import "./ProjectsContainer.scss";
+import React, { Component } from 'react';
+import Project from '../../components/Project/Project';
+import { TransitionGroup } from 'react-transition-group';
+import * as actions from '../../_redux/actions/index';
+import { connect } from 'react-redux';
+import './ProjectsContainer.scss';
 
 class ProjectsContainer extends Component {
-  state = {
-    user: { userId: "123ddEad2199E", username: "Elliot" }
-  };
+	state = {
+		user: { userId: '123ddEad2199E', username: 'Elliot' }
+	};
 
-  componentDidMount() {
-    this.props.addUserProjects([
-      {
-        projectTitle: "--Send to Steve",
-        projectId: "asd3213ey1u",
-        projectDesc: "Palettes that Steve requested ...",
-        palettes: [
-          {
-            paletteTitle: "Ok here is title",
-            paletteColors: ["#45CD12", "#DFD442", "#4E3211"],
-            paletteId: "asd3a1DD3sd"
-          },
-          {
-            paletteTitle: "Idea - 2 Colors",
-            paletteColors: ["#123456"],
-            paletteId: "eeEE123D3sd"
-          },
-          {
-            paletteTitle: "Serene",
-            paletteColors: ["#FEDCBA", "#45CD12", "#3A3A3A", "#123123"],
-            paletteId: "f321DFDff3sd"
-          },
-          {
-            paletteTitle: "-- Send to Steve",
-            paletteColors: [
-              "#FEDCBA",
-              "#FEDFED",
-              "#ABCABC",
-              "#123123",
-              "#3A3A3A"
-            ],
-            paletteId: "eee23aOO3sd"
-          },
-          {
-            paletteTitle: "Iteration 4",
-            paletteColors: [
-              "#FEDCBA",
-              "#FFFFDD",
-              "#45CD12",
-              "#AABBE1",
-              "#3A3A3A"
-            ],
-            paletteId: "13ASeeU98MN"
-          },
-          {
-            paletteTitle: "- Two Potential Options -",
-            paletteColors: ["#AFFFDD", "#1A3A3A"],
-            paletteId: "13AS3U98MN"
-          }
-        ]
-      },
-      {
-        projectTitle: "My Favorites!",
-        projectId: "3ykuyOP23eeE",
-        projectDesc: "A list of my favorite palettes.",
-        palettes: [
-          {
-            paletteTitle: "For Mary",
-            paletteColors: ["#568111", "#512612", "#EA31EA"],
-            paletteId: "oi112uTR3y"
-          },
-          {
-            paletteTitle: "Idea - ???",
-            paletteColors: ["#123456", "#99EE32"],
-            paletteId: "833AS3U99M"
-          },
-          {
-            paletteTitle: "Iteration 2",
-            paletteColors: ["#FEDCBA", "#FFFFDD", "#AABBE1", "#3A3A3A"],
-            paletteId: "13AS3U98MN"
-          }
-        ]
-      }
-    ]);
-  }
+	// componentDidMount() {
+	//   this.props.addUserProjects([
+	//     {
+	//       projectTitle: "--Send to Steve",
+	//       projectId: "asd3213ey1u",
+	//       projectDesc: "Palettes that Steve requested ...",
+	//       palettes: [
+	//         {
+	//           paletteTitle: "Ok here is title",
+	//           paletteColors: ["#45CD12", "#DFD442", "#4E3211"],
+	//           paletteId: "asd3a1DD3sd"
+	//         },
+	//         {
+	//           paletteTitle: "Idea - 2 Colors",
+	//           paletteColors: ["#123456"],
+	//           paletteId: "eeEE123D3sd"
+	//         },
+	//         {
+	//           paletteTitle: "Serene",
+	//           paletteColors: ["#FEDCBA", "#45CD12", "#3A3A3A", "#123123"],
+	//           paletteId: "f321DFDff3sd"
+	//         },
+	//         {
+	//           paletteTitle: "-- Send to Steve",
+	//           paletteColors: [
+	//             "#FEDCBA",
+	//             "#FEDFED",
+	//             "#ABCABC",
+	//             "#123123",
+	//             "#3A3A3A"
+	//           ],
+	//           paletteId: "eee23aOO3sd"
+	//         },
+	//         {
+	//           paletteTitle: "Iteration 4",
+	//           paletteColors: [
+	//             "#FEDCBA",
+	//             "#FFFFDD",
+	//             "#45CD12",
+	//             "#AABBE1",
+	//             "#3A3A3A"
+	//           ],
+	//           paletteId: "13ASeeU98MN"
+	//         },
+	//         {
+	//           paletteTitle: "- Two Potential Options -",
+	//           paletteColors: ["#AFFFDD", "#1A3A3A"],
+	//           paletteId: "13AS3U98MN"
+	//         }
+	//       ]
+	//     },
+	//     {
+	//       projectTitle: "My Favorites!",
+	//       projectId: "3ykuyOP23eeE",
+	//       projectDesc: "A list of my favorite palettes.",
+	//       palettes: [
+	//         {
+	//           paletteTitle: "For Mary",
+	//           paletteColors: ["#568111", "#512612", "#EA31EA"],
+	//           paletteId: "oi112uTR3y"
+	//         },
+	//         {
+	//           paletteTitle: "Idea - ???",
+	//           paletteColors: ["#123456", "#99EE32"],
+	//           paletteId: "833AS3U99M"
+	//         },
+	//         {
+	//           paletteTitle: "Iteration 2",
+	//           paletteColors: ["#FEDCBA", "#FFFFDD", "#AABBE1", "#3A3A3A"],
+	//           paletteId: "13AS3U98MN"
+	//         }
+	//       ]
+	//     }
+	//   ]);
+	// }
 
-  removeProject = id => {
-    this.props.removeUserProject(id);
-  };
+	removeProject = (id) => {
+		this.props.removeUserProject(id);
+	};
 
-  render() {
-    const projects = this.props.userProjects.map((project, index) => (
-      <Project
-        title={project.projectTitle}
-        project={project}
-        index={index}
-        palettes={project.palettes}
-        key={project.projectId}
-        id={project.projectId}
-        removeProject={this.removeProject}
-      />
-    ));
-    const textMessage = this.props.userProjects.length ? (
-      <p>Here are your projects!</p>
-    ) : (
-      <p>This area is where your projects will be saved!</p>
-    );
-    return (
-      <section
-        className={
-          this.props.userProjects.length
-            ? "ProjectsContainer"
-            : "ProjectsContainer collapse"
-        }
-      >
-        <header className="projects-container-header">{textMessage}</header>
-        <TransitionGroup>{projects}</TransitionGroup>
-      </section>
-    );
-  }
+	render() {
+		const projects = this.props.userProjects.map((project, index) => (
+			<Project
+				title={project.projectTitle}
+				project={project}
+				index={index}
+				palettes={project.palettes}
+				key={project.projectId}
+				id={project.projectId}
+				removeProject={this.removeProject}
+			/>
+		));
+		const textMessage = this.props.userProjects.length ? (
+			<p>Here are your projects!</p>
+		) : (
+			<p>This area is where your projects will be saved!</p>
+		);
+		return (
+			<section className={this.props.userProjects.length ? 'ProjectsContainer' : 'ProjectsContainer collapse'}>
+				<header className="projects-header">
+					<h5>{textMessage}</h5>
+				</header>
+				{/* <TransitionGroup>{projects}</TransitionGroup> */}
+			</section>
+		);
+	}
 }
 
-const mapStateToProps = store => ({
-  userProjects: store.userProjects
+const mapStateToProps = (store) => ({
+	userProjects: store.userProjects
 });
 
-const mapDispatchToProps = dispatch => ({
-  addUserProjects: projects => dispatch(actions.addUserProjects(projects)),
-  removeUserProject: id => dispatch(actions.removeUserProject(id))
+const mapDispatchToProps = (dispatch) => ({
+	addUserProjects: (projects) => dispatch(actions.addUserProjects(projects)),
+	removeUserProject: (id) => dispatch(actions.removeUserProject(id))
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ProjectsContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectsContainer);
