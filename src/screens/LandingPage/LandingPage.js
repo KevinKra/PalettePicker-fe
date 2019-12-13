df91144df66d1a3c1c247d77c351d45a284e67a5
Kevin Krato <kratok08@gmail.com>
2019-12-13 13:11:57 -0700

    Create a directory for screens, create a directory for sass styles

---
+++ src/screens/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProjectsContainer from '../../containers/ProjectsContainer/ProjectsContainer';
+import PalettePicker from '../../containers/PalettePicker/PalettePicker';
+import './LandingPage.scss';
+
+export default function LandingPage() {
+	return (
+		<main className="LandingPage">
+			<PalettePicker />
+			<ProjectsContainer />
+		</main>
+	);
+}
