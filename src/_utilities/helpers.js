ca05be04ff5489d28c6e3b0c041f1f0ab28afe60
Kevin Krato <kratok08@gmail.com>
2019-12-14 07:16:06 -0700

    Add randomHueGenerator function to helpers and import to PalettePicker

--- src/_utilities/helpers.js
+++ src/_utilities/helpers.js
@@ -1,24 +1,28 @@
-export const parseProjects = projects => {
-  return projects.map(project => {
-    if (!project.palettes[0].id) {
-      return {
-        projectTitle: project.title,
-        projectId: project.id,
-        projectDesc: project.description,
-        palettes: []
-      };
-    }
-    return {
-      projectTitle: project.title,
-      projectId: project.id,
-      projectDesc: project.description,
-      palettes: project.palettes.map(palette => {
-        return {
-          paletteTitle: palette.name,
-          paletteColors: palette.colors,
-          paletteId: palette.id
-        };
-      })
-    };
-  });
+export const parseProjects = (projects) => {
+	return projects.map((project) => {
+		if (!project.palettes[0].id) {
+			return {
+				projectTitle: project.title,
+				projectId: project.id,
+				projectDesc: project.description,
+				palettes: []
+			};
+		}
+		return {
+			projectTitle: project.title,
+			projectId: project.id,
+			projectDesc: project.description,
+			palettes: project.palettes.map((palette) => {
+				return {
+					paletteTitle: palette.name,
+					paletteColors: palette.colors,
+					paletteId: palette.id
+				};
+			})
+		};
+	});
+};
+
+export const generateRandomHue = () => {
+	return Math.floor(Math.random() * (360 + 1));
 };
